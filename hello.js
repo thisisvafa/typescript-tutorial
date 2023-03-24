07ffbd75d6b7218ef268c0efa6790e648ea2e434
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-24 18:44:12 +0430

    Type Interface

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-var userName = 69;
+// const userName: string = 69;
+// ====================================================================================================
+// let userName = 'Vafa';
+// userName = 69;
+// tsc --noEmitOnError hello.ts   =>   type in terminal, show the error message
+// ====================================================================================================
+var userName = true;
+var newUser = userName;
--- hello.ts
+++ hello.ts
@@ -1,1 +1,12 @@
-const userName: string = 69;
+// const userName: string = 69;
+
+// ====================================================================================================
+
+// let userName = 'Vafa';
+// userName = 69;
+// tsc --noEmitOnError hello.ts   =>   type in terminal, show the error message
+
+// ====================================================================================================
+
+let userName = true;
+let newUser = userName;
