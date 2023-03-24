fae03231d6d155af9226037e6680488128b4b5ec
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-24 20:00:55 +0430

    Types for Objects and Arrays

---
+++ app.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "John",
+    age: 28,
+    isMarried: false,
+    hubbies: ["playing basketball", "watching tv"]
+};
---
+++ app.ts
@@ -0,0 +1,11 @@
+const user: {
+    name: string;
+    age: number;
+    isMarried: boolean;
+    hubbies: string[];
+} = {
+    name: "John",
+    age: 28,
+    isMarried: false,
+    hubbies: ["playing basketball","watching tv"]
+}
